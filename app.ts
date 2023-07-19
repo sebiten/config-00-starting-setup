2ac680874472996e430d9cc3e1400084c4e12073
Sebiten <sebdevspace@gmail.com>
2023-07-19 11:24:30 -0300

    more compiler data

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
-console.log('Time to get started...');
+console.log("Time to get started...");
 
-const userName = "Sebastian"
+const button = document.querySelector("button")!;
+
+button.addEventListener('click', () => {
+  console.log("Clicked!!");
+  
+})
+// compiler options
+const userName = "Sebastian";
 
 console.log(userName);
